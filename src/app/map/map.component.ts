a1386702853f6207413507297d09747cd5ce5664
sarah <eumieyi@gmail.com>
2019-12-11 18:55:21 -0500

    google maps api working

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
+})
+export class MapComponent implements OnInit {
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: "hybrid",
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8
+  };
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--;
+  }
+}
