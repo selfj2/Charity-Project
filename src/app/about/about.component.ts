49c50139ea8e95bea93ce88de72e5ac564f848af
Oxana <oxanafly@yahoo.com>
2019-12-12 19:48:43 -0500

    Merge with master

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  navbarOpen = false;
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
   constructor() { }
 
   ngOnInit() {
