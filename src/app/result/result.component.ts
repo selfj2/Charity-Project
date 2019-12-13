6119f35eabe1826071bf81ec7e3e659350dcefba
Oxana <oxanafly@yahoo.com>
2019-12-12 20:04:09 -0500

    more css

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { SearchService } from "../search.service";
-import { Router } from "@angular/router";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ApiService } from "../api.service";
+import { MapInfoWindow, MapMarker } from "@angular/google-maps";
 
 @Component({
   selector: "app-result",
@@ -29,4 +28,70 @@ export class ResultComponent implements OnInit {
       });
     });
   }
+
+  charity: any;
+
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: "hybrid",
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8
+  };
+
+  
+
+  ngAfterViewInit() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+        lat: 41.8781,
+        // position.coords.latitude,
+        lng: -87.623177
+        // position.coords.longitude
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--;
+  }
+
+  markers: any[] = [];
+
+  createMarkers() {
+    if (this.charity && this.charity.data) {
+      this.markers = [
+        {
+          position: {
+            lat: this.charity.data.location.coordinates[1],
+            lng: this.charity.data.location.coordinates[0]
+          },
+          label: {
+            color: "yellow",
+            text: this.charity.data.city
+          },
+          title: "Marker title",
+          info: "Marker info " + (this.markers.length + 1),
+          options: { animation: google.maps.Animation.BOUNCE }
+        }
+      ];
+    }
+  }
+
+  openInfo(marker: MapMarker, content) {
+    this.charity = content;
+    this.charity.open(marker);
+  }
+
+  ngOnChanges() {
+    this.createMarkers();
+  }
 }
+
